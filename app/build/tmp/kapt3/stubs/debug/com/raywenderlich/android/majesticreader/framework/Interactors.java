cdc76594a2567e5d4138fae1ed9766760205e41a
marouene <Marouene@3ci.fr>
2020-02-20 15:09:32 +0100

    vcs

--- app/build/tmp/kapt3/stubs/debug/com/raywenderlich/android/majesticreader/framework/Interactors.java
+++ app/build/tmp/kapt3/stubs/debug/com/raywenderlich/android/majesticreader/framework/Interactors.java
@@ -2,10 +2,74 @@ package com.raywenderlich.android.majesticreader.framework;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 1, 15}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/raywenderlich/android/majesticreader/framework/Interactors;", "", "()V", "app_debug"})
+@kotlin.Metadata(mv = {1, 1, 15}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000<\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0012\u0018\u00002\u00020\u0001BE\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u0012\u0006\u0010\u0006\u001a\u00020\u0007\u0012\u0006\u0010\b\u001a\u00020\t\u0012\u0006\u0010\n\u001a\u00020\u000b\u0012\u0006\u0010\f\u001a\u00020\r\u0012\u0006\u0010\u000e\u001a\u00020\u000f\u0012\u0006\u0010\u0010\u001a\u00020\u0011\u00a2\u0006\u0002\u0010\u0012R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u0014R\u0011\u0010\b\u001a\u00020\t\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0016R\u0011\u0010\u0006\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0017\u0010\u0018R\u0011\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u001aR\u0011\u0010\n\u001a\u00020\u000b\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001b\u0010\u001cR\u0011\u0010\u000e\u001a\u00020\u000f\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001d\u0010\u001eR\u0011\u0010\f\u001a\u00020\r\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001f\u0010 R\u0011\u0010\u0010\u001a\u00020\u0011\u00a2\u0006\b\n\u0000\u001a\u0004\b!\u0010\"\u00a8\u0006#"}, d2 = {"Lcom/raywenderlich/android/majesticreader/framework/Interactors;", "", "addBookmark", "LAddBookmark;", "getBookmarks", "LGetBookmarks;", "deleteBookmark", "Lcom/raywenderlich/android/majesticreader/interactors/RemoveBookmark;", "addDocument", "Lcom/raywenderlich/android/majesticreader/interactors/AddDocument;", "getDocuments", "LGetDocuments;", "removeDocument", "Lcom/raywenderlich/android/majesticreader/interactors/RemoveDocument;", "getOpenDocument", "LGetOpenDocument;", "setOpenDocument", "Lcom/raywenderlich/android/majesticreader/interactors/SetOpenDocument;", "(LAddBookmark;LGetBookmarks;Lcom/raywenderlich/android/majesticreader/interactors/RemoveBookmark;Lcom/raywenderlich/android/majesticreader/interactors/AddDocument;LGetDocuments;Lcom/raywenderlich/android/majesticreader/interactors/RemoveDocument;LGetOpenDocument;Lcom/raywenderlich/android/majesticreader/interactors/SetOpenDocument;)V", "getAddBookmark", "()LAddBookmark;", "getAddDocument", "()Lcom/raywenderlich/android/majesticreader/interactors/AddDocument;", "getDeleteBookmark", "()Lcom/raywenderlich/android/majesticreader/interactors/RemoveBookmark;", "getGetBookmarks", "()LGetBookmarks;", "getGetDocuments", "()LGetDocuments;", "getGetOpenDocument", "()LGetOpenDocument;", "getRemoveDocument", "()Lcom/raywenderlich/android/majesticreader/interactors/RemoveDocument;", "getSetOpenDocument", "()Lcom/raywenderlich/android/majesticreader/interactors/SetOpenDocument;", "app_debug"})
 public final class Interactors {
+    @org.jetbrains.annotations.NotNull()
+    private final AddBookmark addBookmark = null;
+    @org.jetbrains.annotations.NotNull()
+    private final GetBookmarks getBookmarks = null;
+    @org.jetbrains.annotations.NotNull()
+    private final com.raywenderlich.android.majesticreader.interactors.RemoveBookmark deleteBookmark = null;
+    @org.jetbrains.annotations.NotNull()
+    private final com.raywenderlich.android.majesticreader.interactors.AddDocument addDocument = null;
+    @org.jetbrains.annotations.NotNull()
+    private final GetDocuments getDocuments = null;
+    @org.jetbrains.annotations.NotNull()
+    private final com.raywenderlich.android.majesticreader.interactors.RemoveDocument removeDocument = null;
+    @org.jetbrains.annotations.NotNull()
+    private final GetOpenDocument getOpenDocument = null;
+    @org.jetbrains.annotations.NotNull()
+    private final com.raywenderlich.android.majesticreader.interactors.SetOpenDocument setOpenDocument = null;
     
-    public Interactors() {
+    @org.jetbrains.annotations.NotNull()
+    public final AddBookmark getAddBookmark() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final GetBookmarks getGetBookmarks() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.raywenderlich.android.majesticreader.interactors.RemoveBookmark getDeleteBookmark() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.raywenderlich.android.majesticreader.interactors.AddDocument getAddDocument() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final GetDocuments getGetDocuments() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.raywenderlich.android.majesticreader.interactors.RemoveDocument getRemoveDocument() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final GetOpenDocument getGetOpenDocument() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.raywenderlich.android.majesticreader.interactors.SetOpenDocument getSetOpenDocument() {
+        return null;
+    }
+    
+    public Interactors(@org.jetbrains.annotations.NotNull()
+    AddBookmark addBookmark, @org.jetbrains.annotations.NotNull()
+    GetBookmarks getBookmarks, @org.jetbrains.annotations.NotNull()
+    com.raywenderlich.android.majesticreader.interactors.RemoveBookmark deleteBookmark, @org.jetbrains.annotations.NotNull()
+    com.raywenderlich.android.majesticreader.interactors.AddDocument addDocument, @org.jetbrains.annotations.NotNull()
+    GetDocuments getDocuments, @org.jetbrains.annotations.NotNull()
+    com.raywenderlich.android.majesticreader.interactors.RemoveDocument removeDocument, @org.jetbrains.annotations.NotNull()
+    GetOpenDocument getOpenDocument, @org.jetbrains.annotations.NotNull()
+    com.raywenderlich.android.majesticreader.interactors.SetOpenDocument setOpenDocument) {
         super();
     }
 }
